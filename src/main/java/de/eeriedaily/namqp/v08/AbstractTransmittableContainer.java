bc2071b74b0f11caf204e0e71df2eaa7679b9c57
Joern Barthel <joern.barthel@acm.org>
2010-04-11 18:50:13 +0200

    Added container class.

---
+++ src/main/java/de/eeriedaily/namqp/v08/AbstractTransmittableContainer.java
@@ -0,0 +1,28 @@
+package de.eeriedaily.namqp.v08;
+
+import org.jboss.netty.buffer.ChannelBuffer;
+
+/**
+ * @author Joern Barthel <joern.barthel@acm.org>
+ */
+public abstract class AbstractTransmittableContainer extends AbstractTransmittable {
+
+    protected static Transmittable[] all(Transmittable... transmittables) {
+        return transmittables;
+    }
+
+    protected abstract Transmittable[] getTransmittables();
+
+    public long getSize() {
+        return sizeOf(getTransmittables());
+    }
+
+    public int getSizeAsInt() {
+        return checkIntOverflow(getSize());
+    }
+
+    public void writeTo(ChannelBuffer channelBuffer) {
+        writeTo(channelBuffer, getTransmittables());
+    }
+
+}
