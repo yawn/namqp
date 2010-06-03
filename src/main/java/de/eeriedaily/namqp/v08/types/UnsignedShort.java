ab60705f8a80257ec10bdd81e6c83e21d4062fa6
Joern Barthel <joern.barthel@acm.org>
2010-06-03 19:14:10 +0200

    Added on-demand valueOf cache for unsigned shorts.

--- src/main/java/de/eeriedaily/namqp/v08/types/UnsignedShort.java
+++ src/main/java/de/eeriedaily/namqp/v08/types/UnsignedShort.java
@@ -1,7 +1,11 @@
 package de.eeriedaily.namqp.v08.types;
 
+import com.google.common.base.Function;
+import com.google.common.collect.MapMaker;
 import de.eeriedaily.namqp.v08.AbstractTransmittable;
 import org.jboss.netty.buffer.ChannelBuffer;
+
+import java.util.Map;
 
 /**
  * @author Joern Barthel <joern.barthel@acm.org>
@@ -11,6 +15,20 @@ public class UnsignedShort extends AbstractTransmittable implements Type {
     public static final long SIZE = 2;
 
     private final int unsignedShort;
+
+    private static final Map<Integer, UnsignedShort> cache = new MapMaker()
+            .softValues()
+            .makeComputingMap(new Function<Integer, UnsignedShort>() {
+
+                public UnsignedShort apply(Integer from) {
+                    return new UnsignedShort(from);
+                }
+
+            });
+
+    public static UnsignedShort valueOf(int unsignedShort) {
+        return cache.get(unsignedShort);
+    }
 
     public UnsignedShort(int unsignedShort) {
         this.unsignedShort = unsignedShort;
