b8c902a36806dcddd8192eb15f3f2e13861be7e8
Joern Barthel <joern.barthel@acm.org>
2010-06-03 19:24:18 +0200

    Using pipeline factory in client. Using amqp channels in client.

--- src/main/java/de/eeriedaily/namqp/v08/Client.java
+++ src/main/java/de/eeriedaily/namqp/v08/Client.java
@@ -1,14 +1,20 @@
 package de.eeriedaily.namqp.v08;
 
+import com.google.common.util.concurrent.ListenableFuture;
 import de.eeriedaily.namqp.v08.handler.*;
+import de.eeriedaily.namqp.v08.handler.channel.ChannelDelegation;
+import de.eeriedaily.namqp.v08.methods.connection.ConnectionOpenOk;
+import de.eeriedaily.namqp.v08.types.UnsignedShort;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.jboss.netty.bootstrap.ClientBootstrap;
-import org.jboss.netty.channel.Channel;
-import org.jboss.netty.channel.ChannelFuture;
 import org.jboss.netty.channel.ChannelPipeline;
+import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.channel.Channels;
 
 import java.net.InetSocketAddress;
+
+import static de.eeriedaily.namqp.v08.framing.Frame.ADMIN_CHANNEL;
 
 /**
  * @author Joern Barthel <joern.barthel@acm.org>
@@ -19,39 +25,53 @@ public class Client {
 
     private final ClientBootstrap bootstrap;
     private final Configuration configuration;
-
-    private Channel channel;
+    private final ChannelDelegation channelDelegation;
 
     public Client(ClientBootstrap bootstrap, Configuration configuration) {
         this.bootstrap = bootstrap;
         this.configuration = configuration;
+        this.channelDelegation = new ChannelDelegation();
     }
 
-    public ChannelFuture connect() {
+    public ListenableFuture<ConnectionOpenOk> connect() {
 
         if (log.isInfoEnabled())
             log.info(String.format("Connecting with configuration '%s'", configuration));
 
-        ChannelPipeline pipeline = bootstrap.getPipeline();
-        pipeline.addLast("assembler", new FrameAssembler());
-        pipeline.addLast("decoder", new FrameDecoder());
+        bootstrap.setPipelineFactory(new ChannelPipelineFactory() {
 
-        if (configuration.getHeartbeat() > 0)
-            pipeline.addLast("heartbeat", new HeartbeatHandler(configuration));
+            public ChannelPipeline getPipeline() throws Exception {
 
-        pipeline.addLast("handshakeHandler", new HandshakeHandler(configuration));
+                ChannelPipeline pipeline = Channels.pipeline();
 
-        ChannelFuture future = bootstrap.connect(new InetSocketAddress(configuration.getBrokerHostname(),
+                // downstream
+                pipeline.addLast("encoder", new FrameEncoder());
+                
+                // upstream
+                pipeline.addLast("assembler", new FrameAssembler());
+                pipeline.addLast("decoder", new FrameDecoder());
+                pipeline.addLast("heartbeatHandler", new HeartbeatHandler(configuration));
+                pipeline.addLast("channels", channelDelegation);
+                pipeline.addLast("handshakeHandler", new HandshakeHandler(configuration,
+                        channelDelegation.getChannel(ADMIN_CHANNEL)));
+
+                return pipeline;
+
+            }
+
+        });
+
+        ListenableFuture<ConnectionOpenOk> future = getChannel(ADMIN_CHANNEL).addMethodListener(ConnectionOpenOk.class);
+
+        bootstrap.connect(new InetSocketAddress(configuration.getBrokerHostname(),
                 configuration.getBrokerPort()));
-
-        this.channel = future.awaitUninterruptibly().getChannel();
 
         return future;
 
     }
 
-    public Channel getChannel() {
-        return channel;
+    public de.eeriedaily.namqp.v08.handler.channel.Channel getChannel(UnsignedShort channel) {
+        return channelDelegation.getChannel(channel);
     }
 
 }
