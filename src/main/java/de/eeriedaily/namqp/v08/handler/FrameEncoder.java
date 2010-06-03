21d97d5e5a24fb0f109c4ab74d95213c3f6bb6c4
Joern Barthel <joern.barthel@acm.org>
2010-06-03 19:17:54 +0200

    Ignore non-frames instead of using install logic.

--- src/main/java/de/eeriedaily/namqp/v08/handler/FrameEncoder.java
+++ src/main/java/de/eeriedaily/namqp/v08/handler/FrameEncoder.java
@@ -7,12 +7,10 @@ import org.jboss.netty.buffer.ChannelBuffer;
 import org.jboss.netty.buffer.ChannelBuffers;
 import org.jboss.netty.channel.Channel;
 import org.jboss.netty.channel.ChannelHandlerContext;
-import org.jboss.netty.channel.ChannelPipeline;
 import org.jboss.netty.handler.codec.oneone.OneToOneEncoder;
 
 /**
- * Encodes frames into a channel buffer. This encoder is installed by the HandshakeHandler, after sending
- * the initial protocol frame.
+ * Encodes frames into a channel buffer.
  *
  * @see de.eeriedaily.namqp.v08.handler.HandshakeHandler
  * @see de.eeriedaily.namqp.v08.framing.Frame
@@ -22,12 +20,11 @@ public class FrameEncoder extends OneToOneEncoder {
 
     private static final Log log = LogFactory.getLog(FrameEncoder.class);
 
-    public FrameEncoder(ChannelPipeline pipeline) {
-        pipeline.addFirst("encoder", this);
-    }
-
     @Override
     protected Object encode(ChannelHandlerContext ctx, Channel channel, Object msg) throws Exception {
+
+        if (msg instanceof ChannelBuffer)
+            return msg;
 
         Frame frame = (Frame) msg;
 
